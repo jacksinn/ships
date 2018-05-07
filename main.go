d33d7bc46c89ec005c481ed09c15e8065e041463
Steven <steven@rootedlogics.com>
2018-05-07 13:17:21 -0400

    adding in some extra ship info

--- main.go
+++ main.go
@@ -8,8 +8,21 @@ import (
 type Ship struct {
 	Name        string
 	Description string
+	MaxWeapons  uint
+	Class       string // Cruiser, Frigate, etc
+	MaxHP       uint
+}
+
+// Weapon stores information about a weapon
+type Weapon struct {
+	Name        string
+	Description string
+	Cost        uint
+	Power       uint
+	Damage      uint
+	Range       uint
 }
 
 func main() {
-	fmt.Println(Ship{"Ship 1", "Ship 1 Description"})
+	fmt.Println(Ship{"Ship 1", "Ship 1 Description", 2, "Frigate", 100})
 }
