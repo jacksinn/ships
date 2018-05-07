4e0628af14959ac073e037233fa9f5d06f32e3dc
Steven <steven@rootedlogics.com>
2018-05-07 15:59:59 -0400

    doing multiple automatic rounds

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type Ship struct {
 	MaxWeapons  uint
 	Class       string // Cruiser, Caravel, Frigate, Dreadnaught
 	MaxHP       uint
+	CurrentHP   uint
 	Color       string
 	MaxSpeed    uint
 	MaxAttack   uint
@@ -19,8 +20,28 @@ type Ship struct {
 	// Ship may have shields and other attachments
 }
 
-func (attacker Ship) attack(defender Ship) {
-	fmt.Println(attacker.Name, " is attacking ", defender.Name)
+func (ship *Ship) attack(defender *Ship) {
+	fmt.Println(ship.Name, " is attacking ", defender.Name)
+
+	fmt.Println(ship.Name, "attacks with his weapon dealing", ship.MaxAttack, "damage")
+	fmt.Println("******Pew pew >>>>>>")
+	// Deal the damage to the other player
+	// If the attack does more damage than the defender's ship has, then it should die
+	if ship.MaxAttack >= defender.CurrentHP {
+		defender.CurrentHP = 0
+	} else {
+		defender.takeDamage(ship.MaxAttack)
+	}
+
+	fmt.Println(defender.Name, "has", defender.CurrentHP, "left")
+	if defender.CurrentHP <= 0 {
+		fmt.Println(defender.Name, "'s shipped got frigate wrecked!")
+	}
+	fmt.Println("--------------------------")
+}
+
+func (ship *Ship) takeDamage(damage uint) {
+	ship.CurrentHP = ship.CurrentHP - damage
 }
 
 // Weapon stores information about a weapon
@@ -49,6 +70,7 @@ func main() {
 		MaxWeapons:  2,
 		Class:       "Frigate",
 		MaxHP:       100,
+		CurrentHP:   100,
 		Color:       "Grey",
 		MaxSpeed:    100,
 		MaxAttack:   50,
@@ -63,9 +85,18 @@ func main() {
 		MaxHP:       100,
 		Color:       "Red",
 		MaxSpeed:    100,
+		CurrentHP:   100,
 		MaxAttack:   50,
 	}
 
-	dad.attack(steven)
+	for i := 1; i <= 10; i++ {
+		fmt.Println("===========================")
+		fmt.Println("Round", i, "::FIGHT!!!!!!!!")
+		// Dad Turn 1
+		dad.attack(&steven)
+
+		// Steven turn 1
+		steven.attack(&dad)
+	}
 
 }
